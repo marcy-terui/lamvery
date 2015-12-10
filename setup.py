376931a109fea071b6d3c20475d15735b267053e
Marcy <marcy9114@gmail.com>
2015-12-10 23:28:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,13 +23,14 @@ See: `GitHub <https://github.com/marcy-terui/lamvery/blob/master/README.md>`__
     },
     license="MIT License",
     classifiers=(
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ),
+    keywords='aws lambda',
 )
 
 setup(**setup_options)
